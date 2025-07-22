001b4b5b19cd69c3986bb578b5338563b344a8bf
Valytessa <valytessa@gmail.com>
2025-07-22 08:57:22 +0700

    feat: Add basic functions in TS and JS

---
+++ 02basics/myfunctions.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function addTwo(num) {
+    // return num + 2
+    return "hello";
+}
+function getUpper(val) {
+    return val.toUpperCase();
+}
+function signUpUser(name, email, isPaid) { }
+var loginUser = function (name, email, isPaid) {
+    if (isPaid === void 0) { isPaid = false; }
+};
+var myValue = addTwo(5);
+getUpper("hitesh");
+signUpUser("hitesh", "hitesh@lco.dev", false);
+loginUser("h", "h@h.com");
---
+++ 02basics/myfunctions.ts
@@ -0,0 +1,20 @@
+function addTwo(num: number) {
+    // return num + 2
+    return "hello"
+}
+
+function getUpper(val: string) {
+    return val.toUpperCase()
+}
+
+function signUpUser(name: string, email: string, isPaid: boolean) {}
+
+let loginUser = (name: string, email: string, isPaid: boolean = false) => {}
+
+let myValue = addTwo(5)
+getUpper("hitesh")
+
+signUpUser("hitesh", "hitesh@lco.dev", false)
+loginUser("h", "h@h.com")
+
+export {}
