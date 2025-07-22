5bad869c12bb98e7f9dc19fbb07062365d40760e
Valytessa <valytessa@gmail.com>
2025-07-22 09:27:25 +0700

    fix: Correct return type for addTwo function and add more functions

--- 02basics/myfunctions.ts
+++ 02basics/myfunctions.ts
@@ -1,6 +1,6 @@
-function addTwo(num: number) {
-    // return num + 2
-    return "hello"
+function addTwo(num: number): number {
+    return num + 2
+    // return "hello"
 }
 
 function getUpper(val: string) {
@@ -16,5 +16,35 @@ getUpper("hitesh")
 
 signUpUser("hitesh", "hitesh@lco.dev", false)
 loginUser("h", "h@h.com")
+
+// function getValue(myVal: number): boolean{
+//     if (myVal > 5) {
+//         return true
+//     }
+//     return "200 OK"
+// }
+
+const getHello = (s: string):string => {
+    return ""
+}
+
+const heros = ["thor", "spidenman", "ironman"]
+// const heros = [1, 2, 3]
+
+heros.map((hero): string => {
+    return `hero is ${hero}`
+})
+
+
+function consoleError(errmsg: string): void {
+    console.log(errmsg);
+
+}
+function handleError(errmsg: string): never {
+    throw new Error(errmsg);
+
+}
+
+
 
 export {}
